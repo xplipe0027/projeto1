4a8681ab456a3f5507633f6e2d3fcef56ee76357
xplipe0027 <pllipe.dinamite@gmail.com>
2024-01-26 12:55:57 -0300

    first commit

---
+++ pyrogram/handlers/message_handler.py
@@ -0,0 +1,71 @@
+#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of Pyrogram.
+#
+#  Pyrogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+from typing import Callable
+
+from .handler import Handler
+
+
+class MessageHandler(Handler):
+    """The Message handler class. Used to handle new messages.
+    It is intended to be used with :meth:`~pyrogram.Client.add_handler`
+
+    For a nicer way to register this handler, have a look at the
+    :meth:`~pyrogram.Client.on_message` decorator.
+
+    Parameters:
+        callback (``Callable``):
+            Pass a function that will be called when a new Message arrives. It takes *(client, message)*
+            as positional arguments (look at the section below for a detailed description).
+
+        filters (:obj:`Filters`):
+            Pass one or more filters to allow only a subset of messages to be passed
+            in your callback function.
+
+    Other parameters:
+        client (:obj:`~pyrogram.Client`):
+            The Client itself, useful when you want to call other API methods inside the message handler.
+
+        message (:obj:`~pyrogram.types.Message`):
+            The received message.
+    """
+
+    def __init__(self, callback: Callable, filters=None):
+        self.user_callback = callback
+        super().__init__(self.resolve_listener, filters)
+
+    async def resolve_listener(self, client, message, *args):
+        listener = client.listening.get(message.chat.id)
+        if listener and not listener['future'].done():
+            listener['future'].set_result(message)
+        else:
+            if listener and listener['future'].done():
+                client.clear_listener(message.chat.id, listener['future'])
+            await self.user_callback(client, message, *args)
+
+    async def check(self, client, update):
+        listener = client.listening.get(update.chat.id)
+
+        if listener and not listener['future'].done():
+            return await listener['filters'](client, update) if callable(listener['filters']) else True
+
+        return (
+            await self.filters(client, update)
+            if callable(self.filters)
+            else True
+        )
